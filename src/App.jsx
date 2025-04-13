526d6dd5f8786f785f0b38d8502bf3c22b0805eb
Otirbepilef <otirbepilef@gmail.com>
2025-04-13 12:52:09 -0300

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import List from './components/MainList'
-import MainList from './components/MainList'
-import MarketItens from './components/MarketItens'
-import Input from './components/Input'
+import "./App.css";
+import MainList from "./components/MainList";
 
 function App() {
-  
   return (
     <>
-    <MainList />
-    
+      <MainList />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
